f549923c2cf7442f0f5b48330e8d0812b55a807c
Dharsan <dharsan.s0503@gmail.com>
2024-04-26 21:46:15 +0530

    files-added

---
+++ JAVASCRIPT/PraticeJavascript/script..js
@@ -0,0 +1,71 @@
+// var n = 5;
+// var n = 5; same variableName  can be declared multiple times
+
+// let n =5;
+// let n =5;In let can use the same variableName in same block,in different block 
+
+// const n =10;
+// console.log(n);
+// {
+//     const n = 20;
+//     console.log(n); in same block also const is fixed in different block const same variable can changed
+
+
+// function sum(x,y){
+//     return x+y;   normal function
+// }
+// var number = sum(20,30);
+// console.log(number);
+
+// x = (z,y)=>{
+//     return z+y;    arrow function
+// }
+// var number = x(10,20);
+// console.log(number)
+
+
+// display = (numb)=>{
+//     console.log(numb)   callback function
+// }
+
+// num = (x,y,callback)=>{
+//     n = x+y;
+//     callback(n);
+// }
+
+// var z = num(10,20,display);
+// console.log(z);
+
+// var n = 0          event onclick function
+// function count(){
+//     n += 1
+//     document.getElementById('increment').innerHTML = n;
+// }
+
+// var str = "hi im there";
+// var n = str.length;
+// n = str.charAt(1);
+// n = str.charAt(0).toUpperCase();
+// n = str.slice(1,str.length);
+// n = str.at(1);
+// console.log(n)
+
+
+// let arr = new Array();
+// arr.push(10);
+// arr.push(20);
+// arr.push(30);
+// arr.push(40);
+// arr.push(50);
+// arr.push(60);
+// arr.pop(60);
+
+// // arr.at(1);
+// // n = arr.length;
+// // console.log(n);
+
+// for(let i=0; i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// arr.delete(10);
